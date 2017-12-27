f4f453673617929f0748fc32da7fd9a060315029
Aram <avalbaro@gmail.com>
2017-12-26 22:12:54 -0800

    update

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS todolist;
+DROP DATABASE IF EXISTS phtn224x85wco5s4;
 -- Creates the "todolist" database --
-CREATE DATABASE todolist;
+CREATE DATABASE phtn224x85wco5s4;
